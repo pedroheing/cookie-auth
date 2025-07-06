1122704e72e603e219fb3672f398295f483feb43
Pedro Henrique Heing Geronimo <pedroheing@gmail.com>
2025-07-06 19:25:42 -0300

    feat: adds atomic comands to renew and release lock

--- src/common/distributed-lock/distributed-lock.service.ts
+++ src/common/distributed-lock/distributed-lock.service.ts
@@ -2,18 +2,20 @@ import { Inject, Injectable } from '@nestjs/common';
 import { RedisService } from '../redis/redis.service';
 import { Lock } from './lock';
 import { DistributedLockConfig, distributedLockConfigRegistration } from 'src/config/distributed-lock.config';
+import { v4 as uuidv4 } from 'uuid';
 
 interface DistributedLockOptions {
 	/**
 	 * Max amount of time an item can remain in the queue before acquiring the lock.
 	 *
-	 * @default 0 (Never)
+	 * @default undefined (Never)
 	 */
 	timeout?: number | undefined;
 
 	/**
 	 * Max amount of time a lock should be held for in seconds
 	 *
+	 * @default DistributedLockConfig.expirationTimeInSeconds
 	 */
 	expirationTimeInSeconds?: number | undefined;
 }
@@ -27,7 +29,7 @@ export class DistributedLockService {
 
 	acquire(key: string, options?: DistributedLockOptions): Promise<Lock> {
 		return new Promise<Lock>((resolve, reject) => {
-			const lockValue = 'lock';
+			const lockValue = uuidv4();
 			const lockExpirationTimeInSeconds = options?.expirationTimeInSeconds ?? this.distributedLockConfig.expirationTimeInSeconds;
 			let timeoutCheck: NodeJS.Timeout;
 			let isTimedOut = false;
@@ -42,9 +44,9 @@ export class DistributedLockService {
 						reject(err);
 					} else if (result === 'OK') {
 						clearTimeout(timeoutCheck);
-						resolve(new Lock(this.redisService, key, lockExpirationTimeInSeconds));
+						resolve(new Lock(this.redisService, key, lockValue, lockExpirationTimeInSeconds));
 					} else {
-						setTimeout(acquireLock, 100);
+						setTimeout(acquireLock, 80 + Math.floor(Math.random() * 40)); // Jitter - 80-120 ms
 					}
 				});
 			};
--- src/common/redis/redis.service.ts
+++ src/common/redis/redis.service.ts
@@ -2,6 +2,13 @@ import { Inject, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/commo
 import Redis from 'ioredis';
 import { RedisConfig, redisConfigRegistration } from 'src/config/redis.config';
 
+declare module 'ioredis' {
+	interface Redis {
+		renewLock(key: string, value: string, ttl: number): Promise<number>;
+		releaseLock(key: string, value: string): Promise<number>;
+	}
+}
+
 @Injectable()
 export class RedisService extends Redis implements OnModuleInit, OnModuleDestroy {
 	constructor(@Inject(redisConfigRegistration.KEY) redisConfig: RedisConfig) {
@@ -13,10 +20,34 @@ export class RedisService extends Redis implements OnModuleInit, OnModuleDestroy
 	}
 
 	async onModuleInit() {
+		this.defineLuaCommands();
 		await this.connect();
 	}
 
 	async onModuleDestroy() {
 		await this.quit();
+	}
+
+	private defineLuaCommands(): void {
+		this.defineCommand('renewLock', {
+			numberOfKeys: 1,
+			lua: `
+                if redis.call("get", KEYS[1]) == ARGV[1] then
+                    return redis.call("expire", KEYS[1], ARGV[2])
+                else
+                    return 0
+                end
+            `,
+		});
+		this.defineCommand('releaseLock', {
+			numberOfKeys: 1,
+			lua: `
+                if redis.call("get", KEYS[1]) == ARGV[1] then
+                    return redis.call("del", KEYS[1])
+                else
+                    return 0
+                end
+            `,
+		});
 	}
 }
