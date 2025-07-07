fc2d905f1f1c1d73a0e26d3b6f7b60f8decfd50f
Pedro Henrique Heing Geronimo <pedroheing@gmail.com>
2025-07-07 12:40:11 -0300

    feat: adds function to change password

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Prisma, Session, SessionStatus, User } from '@prisma/client';
 import { RedisService } from 'src/common/redis/redis.service';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -9,6 +9,7 @@ import { PrismaService, PrismaTx } from 'src/common/prisma/prisma.service';
 import { AuthConfig, authConfigRegistration } from 'src/config/auth.config';
 import { PasswordHashingService } from 'src/common/password-hashing/password-hashing.service';
 import { DistributedLockService } from 'src/common/distributed-lock/distributed-lock.service';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 interface CreateSessionResult {
 	session: Session;
@@ -19,6 +20,13 @@ interface ValidateUserSessionResult {
 	userId: number;
 	/**Only has value if token was refreshed */
 	newSessionToken?: string;
+}
+
+interface ChangePasswordInput {
+	userId: number;
+	sessionToken: string;
+	currentPassword: string;
+	newPassword: string;
 }
 
 @Injectable()
@@ -89,13 +97,14 @@ export class AuthService {
 			select: {
 				status: true,
 				token_hash: true,
+				user_id: true,
 			},
 		});
 		if (!session || session.status === SessionStatus.Revoked) {
 			return;
 		}
 		// removes first from cache as is the first to be checked
-		await this.removeSessionFromCache(session.token_hash);
+		await this.removeSessionFromCache(session.user_id, session.token_hash);
 		await this.prismaService.session.update({
 			where: {
 				token_hash: tokenHash,
@@ -107,13 +116,59 @@ export class AuthService {
 		});
 	}
 
+	public async changePassword(input: ChangePasswordInput): Promise<void> {
+		const user = await this.prismaService.user.findUnique({
+			where: {
+				user_id: input.userId,
+			},
+			select: {
+				user_id: true,
+				password: true,
+			},
+		});
+		if (!user) {
+			throw new NotFoundException('User not found');
+		}
+		const isCurrentPasswordValid = await this.passwordHashService.verify(user.password, input.currentPassword);
+		if (!isCurrentPasswordValid) {
+			throw new UnauthorizedException('It was not possible to verify your current password');
+		}
+		const newPassWordHash = await this.passwordHashService.hash(input.newPassword);
+		this.prismaService
+			.$transaction(async (tx: PrismaTx) => {
+				await tx.user.update({
+					where: {
+						user_id: user.user_id,
+					},
+					data: {
+						password: newPassWordHash,
+					},
+				});
+				await tx.session.updateMany({
+					where: {
+						user_id: user.user_id,
+						NOT: {
+							token_hash: this.getSessionTokenHash(input.sessionToken),
+						},
+					},
+					data: {
+						status: SessionStatus.Revoked,
+					},
+				});
+				await this.removeAllUserSessionsFromCache(user.user_id);
+			})
+			.catch(() => {
+				throw new InternalServerErrorException('It was not possible to change your password. Try again later.');
+			});
+	}
+
 	public async validateSession(sessionToken: string): Promise<ValidateUserSessionResult | null> {
 		const tokenhash = this.getSessionTokenHash(sessionToken);
 		return this._validateSession(tokenhash);
 	}
 
 	private async _validateSession(tokenHash: string) {
-		let session = await this.getSessionFromCacheOrDatabase(tokenHash);
+		const session = await this.getSessionFromCacheOrDatabase(tokenHash);
 		if (!session) {
 			return null;
 		}
@@ -138,7 +193,10 @@ export class AuthService {
 				const refreshResult = await this.refreshSessionToken(session.session_id);
 				await this.addSessionToCache(refreshResult.session);
 				// allows older session to be valid for an extra time to finish concurrent calls
-				await this.redisService.expire(this.getRedisSessionKey(oldSessionTokenhash), this.authConfig.authSessionCacheTTLAterTokenRefreshInSeconds);
+				await this.redisService.expire(
+					this.buildRedisSessionKey(session.user_id, oldSessionTokenhash),
+					this.authConfig.authSessionCacheTTLAterTokenRefreshInSeconds,
+				);
 				// stores result of the token refresh for the next concurrent call on the queue
 				await this.redisService.setex(
 					sessionTokenRefreshResultKey,
@@ -156,7 +214,7 @@ export class AuthService {
 	private async getSessionFromCacheOrDatabase(tokenHash: string): Promise<Session | null> {
 		const cachedSession = await this.getSessionFromCache(tokenHash);
 		if (cachedSession) {
-			await this.redisService.expire(this.getRedisSessionKey(tokenHash), this.authConfig.cacheLifespanInSeconds);
+			await this.redisService.expire(this.buildRedisSessionKey(cachedSession.user_id, tokenHash), this.authConfig.cacheLifespanInSeconds);
 			return cachedSession;
 		}
 		const dbSession = await this.prismaService.session.findUnique({
@@ -203,15 +261,24 @@ export class AuthService {
 	}
 
 	private async addSessionToCache(session: Session): Promise<void> {
-		await this.redisService.set(this.getRedisSessionKey(session.token_hash), JSON.stringify(session), 'EX', this.authConfig.cacheLifespanInSeconds);
+		await this.redisService.set(
+			this.buildRedisSessionKey(session.user_id, session.token_hash),
+			JSON.stringify(session),
+			'EX',
+			this.authConfig.cacheLifespanInSeconds,
+		);
 	}
 
-	private async removeSessionFromCache(tokenHash: string): Promise<void> {
-		await this.redisService.del(this.getRedisSessionKey(tokenHash));
+	private async removeSessionFromCache(userId: number, tokenHash: string): Promise<void> {
+		await this.redisService.unlink(this.buildRedisSessionKey(userId, tokenHash));
+	}
+
+	private async removeAllUserSessionsFromCache(userId: number): Promise<void> {
+		await this.redisService.unlinkPattern(this.buildRedisSessionKey(userId, '*'));
 	}
 
 	private async getSessionFromCache(tokenhash: string): Promise<Session | null> {
-		const sessionCache = await this.redisService.get(this.getRedisSessionKey(tokenhash));
+		const sessionCache = await this.redisService.getPattern(this.buildRedisSessionKey('*', tokenhash));
 		if (sessionCache) {
 			return JSON.parse(sessionCache);
 		}
@@ -226,8 +293,8 @@ export class AuthService {
 		return addDays(issuedAt, this.authConfig.sessionLifespanInDays);
 	}
 
-	private getRedisSessionKey(tokenHash: string) {
-		return `session:${tokenHash}`;
+	private buildRedisSessionKey(userId: number | '*', tokenHash: string) {
+		return `session:${userId}:${tokenHash}`;
 	}
 
 	private getSessionTokenHash(sessionToken: string) {
---
+++ src/features/auth/dto/change-password.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class ChangePasswordDto {
+	@IsNotEmpty()
+	@IsString()
+	@MinLength(6)
+	@MaxLength(255)
+	readonly currentPassword: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@MinLength(6)
+	@MaxLength(255)
+	readonly newPassword: string;
+}
