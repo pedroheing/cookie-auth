d4bb22aeaf0ea078f799408e83655b6ab6b0a8fe
Pedro Henrique Heing Geronimo <pedroheing@gmail.com>
2025-08-31 16:56:43 -0300

    refacotr: add readonly to dto

--- src/features/auth/dto/sign-in.dto.ts
+++ src/features/auth/dto/sign-in.dto.ts
@@ -5,11 +5,11 @@ export class SignInDto {
 	@IsString()
 	@MinLength(5)
 	@MaxLength(100)
-	username: string;
+	readonly username: string;
 
 	@IsNotEmpty()
 	@IsString()
 	@MinLength(6)
 	@MaxLength(255)
-	password: string;
+	readonly password: string;
 }
