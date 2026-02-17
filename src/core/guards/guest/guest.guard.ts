4884b244df11e48ab12b716eb610d7ac0dace102
Pedro Henrique Heing Geronimo <pedroheing@gmail.com>
2026-02-17 20:21:16 -0300

    feat: add guest guard

---
+++ src/core/guards/guest/guest.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable, BadRequestException } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthConfigService } from 'src/features/auth/config/auth-config.service';
+
+@Injectable()
+export class GuestGuard implements CanActivate {
+	constructor(private authConfigService: AuthConfigService) {}
+
+	canActivate(context: ExecutionContext): boolean {
+		const req = context.switchToHttp().getRequest() as Request;
+		const cookieName = this.authConfigService.cookie.name;
+		const token = req.cookies?.[cookieName];
+		if (token) {
+			throw new BadRequestException('You are already authenticated. Please sign out first.');
+		}
+		return true;
+	}
+}
